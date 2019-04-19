66a6f542e8cfbe935772b355ce62fcb45310ef96
linlongye <linlongye@126.com>
2019-04-19 18:59:42 +0800

    Initial commit

---
+++ TestLib2/TestForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+
+namespace TestLib2
+{
+  public partial class TestForm : Form
+  {
+    public TestForm()
+    {
+      InitializeComponent();
+      Load += Form1_Load;
+    }
+
+    private ChromiumWebBrowser _webBrowser;
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      _webBrowser =
+        new ChromiumWebBrowser(Path.Combine(Environment.CurrentDirectory, "html/index.html")) {Dock = DockStyle.Fill};
+      Controls.Add(_webBrowser);
+    }
+  }
+}
